700d8957f1e3f7578fff17794edd6b4e7b0ba7f6
Ilya Afanasyev <affeeal@yandex.ru>
2023-12-17 21:14:38 +0300

    finish lab3 main part

---
+++ lab3/lib/utils/include/utils.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <eigen3/Eigen/Dense>
+
+namespace utils {
+
+using Matrix =
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+
+constexpr int kDimension = 5000;
+constexpr double kEpsilon = 1e-5;
+
+Matrix GetTestCoeffs();
+Eigen::VectorXd GetTestFreeCoeffs();
+
+Eigen::VectorXd CalculateSolution(const Matrix& coeffs,
+                                  const Eigen::VectorXd& free_coeffs);
+
+}  // namespace utils
